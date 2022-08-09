8df560348e0b4e8bfdc99a444b9f478f7e68422b
平凡 吴 <1911826283@qq.com>
2022-08-09 22:46:40 +0800

    1.优化了一些已知的问题

--- build-musicplayer-Desktop_Qt_5_9_0_MinGW_32bit-Release/ui_mainwindow.h
+++ build-musicplayer-Desktop_Qt_5_9_0_MinGW_32bit-Release/ui_mainwindow.h
@@ -32,17 +32,17 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QGridLayout *gridLayout;
-    QLabel *currentmusic;
     QPushButton *Bmenu;
-    QPushButton *Bpause;
-    QPushButton *volume_btn;
     QPushButton *Blast;
+    QPushButton *volume_btn;
     QPushButton *Bnext;
-    QLabel *label;
-    QSlider *horizontalSlider;
+    QPushButton *Bpause;
     QSlider *VolumeSlider;
+    QLabel *label;
     QLabel *volume_label;
+    QSlider *horizontalSlider;
     QTableWidget *tableWidget;
+    QLabel *currentmusic;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -60,11 +60,6 @@ public:
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        currentmusic = new QLabel(centralWidget);
-        currentmusic->setObjectName(QStringLiteral("currentmusic"));
-
-        gridLayout->addWidget(currentmusic, 2, 1, 1, 3);
-
         Bmenu = new QPushButton(centralWidget);
         Bmenu->setObjectName(QStringLiteral("Bmenu"));
         QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
@@ -76,14 +71,14 @@ public:
 
         gridLayout->addWidget(Bmenu, 3, 4, 1, 1);
 
-        Bpause = new QPushButton(centralWidget);
-        Bpause->setObjectName(QStringLiteral("Bpause"));
-        Bpause->setCursor(QCursor(Qt::PointingHandCursor));
+        Blast = new QPushButton(centralWidget);
+        Blast->setObjectName(QStringLiteral("Blast"));
+        Blast->setCursor(QCursor(Qt::PointingHandCursor));
         QIcon icon;
-        icon.addFile(QStringLiteral(":/res/start.png"), QSize(), QIcon::Normal, QIcon::Off);
-        Bpause->setIcon(icon);
+        icon.addFile(QStringLiteral(":/res/left.png"), QSize(), QIcon::Normal, QIcon::Off);
+        Blast->setIcon(icon);
 
-        gridLayout->addWidget(Bpause, 3, 2, 1, 1);
+        gridLayout->addWidget(Blast, 3, 1, 1, 1);
 
         volume_btn = new QPushButton(centralWidget);
         volume_btn->setObjectName(QStringLiteral("volume_btn"));
@@ -97,36 +92,23 @@ public:
 
         gridLayout->addWidget(volume_btn, 3, 0, 1, 1);
 
-        Blast = new QPushButton(centralWidget);
-        Blast->setObjectName(QStringLiteral("Blast"));
-        Blast->setCursor(QCursor(Qt::PointingHandCursor));
-        QIcon icon1;
-        icon1.addFile(QStringLiteral(":/res/left.png"), QSize(), QIcon::Normal, QIcon::Off);
-        Blast->setIcon(icon1);
-
-        gridLayout->addWidget(Blast, 3, 1, 1, 1);
-
         Bnext = new QPushButton(centralWidget);
         Bnext->setObjectName(QStringLiteral("Bnext"));
         Bnext->setCursor(QCursor(Qt::PointingHandCursor));
-        QIcon icon2;
-        icon2.addFile(QStringLiteral(":/res/right.png"), QSize(), QIcon::Normal, QIcon::Off);
-        Bnext->setIcon(icon2);
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/res/right.png"), QSize(), QIcon::Normal, QIcon::Off);
+        Bnext->setIcon(icon1);
 
         gridLayout->addWidget(Bnext, 3, 3, 1, 1);
 
-        label = new QLabel(centralWidget);
-        label->setObjectName(QStringLiteral("label"));
+        Bpause = new QPushButton(centralWidget);
+        Bpause->setObjectName(QStringLiteral("Bpause"));
+        Bpause->setCursor(QCursor(Qt::PointingHandCursor));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/res/start.png"), QSize(), QIcon::Normal, QIcon::Off);
+        Bpause->setIcon(icon2);
 
-        gridLayout->addWidget(label, 4, 0, 1, 1);
-
-        horizontalSlider = new QSlider(centralWidget);
-        horizontalSlider->setObjectName(QStringLiteral("horizontalSlider"));
-        horizontalSlider->setCursor(QCursor(Qt::OpenHandCursor));
-        horizontalSlider->setMouseTracking(true);
-        horizontalSlider->setOrientation(Qt::Horizontal);
-
-        gridLayout->addWidget(horizontalSlider, 4, 1, 1, 4);
+        gridLayout->addWidget(Bpause, 3, 2, 1, 1);
 
         VolumeSlider = new QSlider(centralWidget);
         VolumeSlider->setObjectName(QStringLiteral("VolumeSlider"));
@@ -142,6 +124,13 @@ public:
 
         gridLayout->addWidget(VolumeSlider, 2, 0, 1, 1);
 
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setMinimumSize(QSize(72, 0));
+        label->setMaximumSize(QSize(72, 16777215));
+
+        gridLayout->addWidget(label, 4, 0, 1, 1, Qt::AlignRight);
+
         volume_label = new QLabel(centralWidget);
         volume_label->setObjectName(QStringLiteral("volume_label"));
         QSizePolicy sizePolicy3(QSizePolicy::Maximum, QSizePolicy::Maximum);
@@ -151,6 +140,14 @@ public:
         volume_label->setSizePolicy(sizePolicy3);
 
         gridLayout->addWidget(volume_label, 1, 0, 1, 1);
+
+        horizontalSlider = new QSlider(centralWidget);
+        horizontalSlider->setObjectName(QStringLiteral("horizontalSlider"));
+        horizontalSlider->setCursor(QCursor(Qt::OpenHandCursor));
+        horizontalSlider->setMouseTracking(true);
+        horizontalSlider->setOrientation(Qt::Horizontal);
+
+        gridLayout->addWidget(horizontalSlider, 4, 1, 1, 4);
 
         tableWidget = new QTableWidget(centralWidget);
         if (tableWidget->columnCount() < 1)
@@ -168,6 +165,12 @@ public:
         tableWidget->horizontalHeader()->setStretchLastSection(true);
 
         gridLayout->addWidget(tableWidget, 0, 4, 3, 1);
+
+        currentmusic = new QLabel(centralWidget);
+        currentmusic->setObjectName(QStringLiteral("currentmusic"));
+        currentmusic->setMaximumSize(QSize(336, 16777215));
+
+        gridLayout->addWidget(currentmusic, 2, 1, 1, 3);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
@@ -189,19 +192,19 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
-        currentmusic->setText(QString());
         Bmenu->setText(QApplication::translate("MainWindow", "\346\255\214\346\233\262\345\210\227\350\241\250", Q_NULLPTR));
-        Bpause->setText(QString());
+        Blast->setText(QString());
 #ifndef QT_NO_TOOLTIP
         volume_btn->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>\345\215\225\345\207\273\345\274\200\345\220\257\351\235\231\351\237\263</p></body></html>", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
         volume_btn->setText(QString());
-        Blast->setText(QString());
         Bnext->setText(QString());
+        Bpause->setText(QString());
         label->setText(QApplication::translate("MainWindow", "TextLabel", Q_NULLPTR));
         volume_label->setText(QApplication::translate("MainWindow", "TextLabel", Q_NULLPTR));
         QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
         ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\346\255\214\346\233\262\345\220\215\347\247\260", Q_NULLPTR));
+        currentmusic->setText(QString());
     } // retranslateUi
 
 };
